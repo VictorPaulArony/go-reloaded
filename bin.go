0d81bdfd0c0a914efdc923b40955fdbd6e3cd542
victor paul arony <98453453+VictorPaulArony@users.noreply.github.com>
2024-04-17 19:15:09 +0300

    Update bin.go

--- bin.go
+++ bin.go
@@ -20,7 +20,17 @@ func replaceBin(sample string) string {
 	}
 	return strings.Join(words, " ")
 }
-
+// func replaceBin(sample string) string {
+// 	words := strings.Fields(sample) 
+// 	for i, word := range words {
+// 		if strings.Contains(words[i], "(cap)") {
+// 			bin, _ := strconv.ParseInt(wordsi-1], 2, 64 ) 
+// 		}
+// 		words[i-1] = fmt.Sprintf("%d", bin)
+// 		words[i] = ""
+// 	}
+// }
+return strings.Join(words, " " )
 func main() {
 	text := "It has been 01 (bin) years"
 	transformed := replaceBin(text)
