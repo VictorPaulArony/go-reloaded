3018f4a05a93ff3c7cd3d7b51a90fc4e75e10a94
victor paul arony <98453453+VictorPaulArony@users.noreply.github.com>
2024-04-18 11:56:56 +0300

    Update hex.go
    update

--- hex.go
+++ hex.go
@@ -5,18 +5,30 @@ import (
 	"strconv"
 	"strings"
 )
-    func replaceHex (input string) string {
-        words := strings.Fields(input)
-        for i:= 0; i < len(words); i++ {
-            if words[i] == "(hex)" && i > 0 {
-                index, _ := strconv.ParseInt(words[i-1], 16, 64)
-                words[i-1] = fmt.Sprintf("%d", index)
-                words = append(words[:i], words[i+1:]...)
-                i--
-            }
-        }
-        return strings.Join(words, " ")
-    }
+    // func replaceHex (input string) string {
+    //     words := strings.Fields(input)
+    //     for i:= 0; i < len(words); i++ {
+    //         if words[i] == "(hex)" && i > 0 {
+    //             index, _ := strconv.ParseInt(words[i-1], 16, 64)
+    //             words[i-1] = fmt.Sprintf("%d", index)
+    //             words = append(words[:i], words[i+1:]...)
+    //             i--
+    //         }
+    //     }
+    //     return strings.Join(words, " ")
+    // }
+	    func replaceHex(sample string) string {
+		    words := strings.Fields(sample)
+		    for i , word := range words {
+			    if strings.Contains(word, "(hex)" ) {
+				    num, _ := strconv.ParseInt(words[i-1], 16, 64)
+				    words[i-1] = fmt.Sprintf("%d", num)
+				    words = append(words[:i], words[i+1:]...)
+				    }
+			    }
+		    return strings.Join(words, " ")
+		    }
+		    
     func main() {
         text := "look again in page 1E (hex)"
         output :=  replaceHex(text)
